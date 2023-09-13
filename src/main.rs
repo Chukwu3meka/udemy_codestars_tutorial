483018be02ad72cec7144b6427ace325356fee48
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-09-13 16:01:54 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // ? Tuple
+    // * A tuple is a way of grouping a number of values with vaiety of types to one
+    // * Tuple has a fixed length, so once defined cannot grow or shrink in size
+
+    let tup = (500, 'h', true);
+
+    println!("{}", tup.0);
+
+    let (x, y, z) = tup;
+
+    println!("x: {}, y: {}, z: {},", x, y, z);
 }
