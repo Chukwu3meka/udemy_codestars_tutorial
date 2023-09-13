03960f369c7e0cf23115a0bb64eb54bdc7dc3de3
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-09-13 15:17:42 +0100

    immutability in rust using let + mut and const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5;
+
+    // println!("The value of x is {}", x);
+
+    // x = 6;
+
+    // println!("The value of x is {}", x);
+
+    // ?  Mutability using let and mut
+    let mut x = 5;
+
+    println!("The value of x is {}", x);
+
+    x = 6;
+
+    println!("The value of x is {}", x);
+
+    // ? CONST
+    const SECONDS: i8 = 60; // <= data type must be defined
+
+    println!("The value of seconds is {}", SECONDS)
 }
